c398e11301431bcb8bc3a1760c65e5fc8895c193
Diego Linares <linaresbda@gmail.com>
2019-11-28 19:30:24 -0600

    refacor: se mejora estructura de server.js

--- server.js
+++ server.js
@@ -1,36 +1,44 @@
 // dlinares - 28/Nov/2019
 // Se inicia esta prueba de concepto WwebSockets
 // Ejemplo de data que envia el cliente = {"id":"{{idSocket}}","message":"value"}
+
+// Requerimientos
 const express = require('express');
 const socketio = require('socket.io');
 
+// Vars
 const port = 80;
 
-// configuramos servidor web
+// Configuración servidor web (express)
 const app = express();
 app.set("port", port);
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb' }));
-app.disable('x-powered-by');
 
-// iniciamos servidor web
+// Inicia servidor web (express)
 const server = app.listen(app.get('port'), () => {
   console.log(`server start on port ${app.get('port')}`);
 });
 
-// sockets
+// Inicia Sockets
 const io = socketio(server);
+
 // Evento de conección,
 io.on('connect', (socket) => {
   console.log("new connection socketId:", socket.id);
-  //Events
-  socket.on('request', (data) => {
-    // console.log(data);
-    eventResponse(data);
-  });
+  assignEvents(socket);
 })
 
-const eventResponse = (data) => {
-  // io.sockets.emit('response', `Response: ${data}`); // Emmit a todos los sockets
-  io.to(data.id).emit('response', data.message); // Emmit a Socket especifico
-}; 
+// Asigna eventos al socket
+const assignEvents = (socket) => {
+  socket.on('request', (data) => {
+    eventResponseOneToAll(data);
+    eventResponseOneToOne(data);
+  });
+}
+
+// Respuestas
+const eventResponseOneToAll = (data) => {
+  io.sockets.emit('response', `Response: ${data.message}`); // Emmit a todos los sockets
+};
+const eventResponseOneToOne = (data) => {
+  io.to(data.id).emit('response', `Response: ${data.message}`); // Emmit a Socket especifico
+};
